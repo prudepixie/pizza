3c2d53893972c375d30709dcd06fd04c003d6273
Wendy Hsu <wndyhsu@gmail.com>
2014-03-06 11:24:02 -0600

    (testing)

--- spec/pizza_spec.rb
+++ spec/pizza_spec.rb
@@ -10,12 +10,15 @@ describe Topping do
   describe '.initialize' do
       it "sets the name of the topping" do
         topping= Topping.new('olives')
-
         expect(topping.name).to eq('olives')
       end
-    end
-  it "exists" do
-    expect(Topping).to be_a(Class)
 
-  end
+      it "sets whether or not the topping is vegetarian" do
+        topping=Topping.new "bell peppers", vegetarian: true
+        expect(topping.vegetarian).to eq(true)
+      end
+
+      it "exists" do
+        expect(Topping).to be_a(Class)
+      end
 end
